54dd78935a956f730fdf470f420a0e340ea19cd1
hillolborah <hillolborah@rocketmail.com>
2026-06-08 14:58:39 +0530

    vector of threads RPSthreads to vector of futures of type struct PlayerMove.

--- RPS.cpp
+++ RPS.cpp
@@ -3,36 +3,58 @@
 #include <mutex>
 #include <random>
 #include <vector>
+#include <future>
 
 using namespace std;
 
+mutex mu1; //mutex for rng engine -> move
 
-char Player(int PlayerNo){
+struct PlayerMove{
+    int Pid; //PlayerId
+    char move; //moves R(Rock) | P(Paper) | S(Scissor)
+};
+
+PlayerMove Player(int Pid){
 
     char moves[] = {'R', 'P', 'S'};
 
     static random_device rd;
     static mt19937 gen(rd());
-    uniform_int_distribution<> move(0, 2);
+    uniform_int_distribution<> uni_dist(0, 2);
 
-    cout << "PlayerNo : " << PlayerNo << endl;
-    return moves[move(gen)];
+    char move;
+
+    {
+        lock_guard<mutex> lock(mu1);
+        move = moves[uni_dist(gen)];
+        cout << "PlayerId : " << Pid << " -> " << "Move : " << move << endl;
+    }
+
+    return PlayerMove{Pid, move};
 
 }
 
 
+
 int main(){
 
-    cout << "main thread" << endl;
+    cout << "Main Thread" << endl;
 
-    vector<thread> RPSthreads;
-    for(int i = 0; i < 10; i++){
-        RPSthreads.push_back(thread(Player, i));
-        RPSthreads[i].join();
+    const int totalPlayers = 10;
+
+    // vector<thread> RPSthreads; //Threads cannot store structs, replace with vector of futures
+    vector<future<PlayerMove>> RPSFutures;
+
+    for(int i = 0; i < totalPlayers; i++){
+        // RPSthreads.push_back(thread(Player, i));
+        RPSFutures.push_back(async(launch::async, Player, i));
     }
 
-
-
+    for(int i = 0; i < totalPlayers; i++){
+        // RPSthreads[i].join();
+        PlayerMove result = RPSFutures[i].get();
+        cout << "Pid : " << result.Pid << " -> " << "Move : " << result.move <<  endl;
+    }
 
     return 0;
 }
